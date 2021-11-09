e83cccfdea8bf440a99b3ad463fa25fa2fa12ae4
Fabian Kirchmann / BBS2H20A <Fabian.Kirchmann@edu.bib.de>
2021-11-09 09:04:41 +0100

    Table view,navigation controller und edit button

--- TableViews/TableViews/ViewController.swift
+++ TableViews/TableViews/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //datsource
+    var games = ["Far Cry 6","Civ 6","Cod","Battlefield"]
+    
+    
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        
+        navigationItem.rightBarButtonItem = editButtonItem
+        
+        title = "Games"
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    //wie viele zellen sollen angezeigt werden
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    //wie sollen die zellen aussehen
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = games[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate{
+    
+}
